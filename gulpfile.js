cbd1abe0e5249081916af1c69b9bc3e74199a970
Ayana Osawa <ayana.osawa@gmail.com>
2020-02-27 10:49:30 -0500

    Update reset css, gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,82 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
-const imagemin = require('gulp-imagemin');
+// const gulp = require('gulp');
+const { src, dest, task, watch, series, parallel } = require('gulp') //When use this line instead of above line, change method without [gulp.] ex. gulp.dest => dest
+const sass = require('gulp-sass')
+// const concat = require('gulp-concat');
+const minify = require('gulp-minify')
+const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create()
+const imagemin = require('gulp-imagemin')
+const mozjpeg = require('imagemin-mozjpeg')
+const pngquant = require('imagemin-pngquant')
 
-function style() {
-	// sass setting
-	return gulp.src('./_src/scss/app.scss') // where scss files are
-	.pipe(sass({ style: 'compressed' })) // scss function & error setting
-	.pipe(gulp.dest('./dest/css')) //stream changes to all browser
-
-	// browser sync setting
-	.pipe(browserSync.stream())
+// CSS bundle, minify task
+function cssTask() {
+  return (
+    src('./_src/scss/app.scss')
+      // .pipe(sass().on('error', sass.logError))
+      .pipe(
+        sass({
+          outputStyle: 'compressed'
+        }).on('error', sass.logError)
+      )
+      .pipe(autoprefixer())
+      .pipe(dest('./dest/css'))
+      .pipe(browserSync.stream())
+  )
 }
 
-function image() {
-	gulp.src('./_src/images/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('./_src/min_images/'));
+// JS minify task
+function jsTask() {
+  return (
+    src('./_src/js/**/*.js')
+      // .pipe(concat('app.js'))
+      .pipe(
+        minify({
+          ext: {
+            min: '.js'
+          },
+          ignoreFiles: ['-min.js']
+        })
+      )
+      .pipe(dest('./dest/js/'))
+  )
 }
 
-function watch() {
-	browserSync.init({
-		server: {
-			baseDir: './dest/'
-		}
-	})
-	gulp.watch('./_src/images/*', imagemin);
-	gulp.watch('./_src/scss/**/*.scss', style);
-	gulp.watch('./dest/*.html').on('change', browserSync.reload);
-	gulp.watch('./dest/js/*.js').on('change', browserSync.reload);
+// Image minify task
+function imageTask() {
+  return src('./_src/images/*')
+    .pipe(
+      imagemin([
+        pngquant({
+          quality: [0.3, 0.5] // [minimum, maximum]
+        }),
+        mozjpeg({
+          quality: 40
+        })
+      ])
+    )
+    .pipe(dest('./dest/images/'))
 }
 
-exports.style = style;
-exports.image = image;
-exports.watch = watch;
+// Watch task
+function watchTask() {
+  browserSync.init({
+    server: {
+      baseDir: './dest/' //Destination folder
+    }
+  })
+  // watch('./_src/images/*', imagemin); //Use when needed
+  watch('./_src/scss/**/*.scss', cssTask)
+  watch('./_src/js/*.js', series(jsTask, browserSync.reload))
+  watch('./dest/*.html').on('change', browserSync.reload)
+  // watch('./dest/js/*.js').on('change', browserSync.reload); //Use when change js files directly
+}
+
+// Export tasks
+exports.image = imageTask
+exports.style = cssTask
+exports.js = jsTask
+exports.watch = watchTask
+
+// Default task
+exports.default = series(parallel(cssTask, jsTask), watchTask)
