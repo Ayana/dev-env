be41d2a1e77b2f1afb1dbb2af1b063d393726f78
Ayana Osawa <ayana.osawa@gmail.com>
2020-03-07 12:05:20 -0500

    Add viewport setting script

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,75 +1,18 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = _default;
+console.log('Hello World'); // viewport setting
 
-/**
- *
- */
-function _default() {
-  if (navigator.userAgent.match(/Trident\/7\./)) {
-    var onWheel = function onWheel(e) {
-      if (!e) e = window.event; //for legacy IE
-
-      e.preventDefault();
-      var wheelDelta = e.wheelDelta;
-      var currentScrollPosition = window.pageYOffset;
-      window.scrollTo(0, currentScrollPosition - wheelDelta);
-    };
-
-    // if IE
-    var mousewheelevent = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-
-    try {
-      document.addEventListener(mousewheelevent, onWheel, false);
-    } catch (e) {
-      //for legacy IE
-      document.attachEvent('onmousewheel', onWheel);
-    }
+function setViewport() {
+  if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0) {
+    document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=device-width,initial-scale=1.0,user-scalable=yes');
+  } else {
+    document.querySelector("meta[name='viewport']").setAttribute('content', 'width=1286px,user-scalable=auto');
   }
 }
 
-$(function () {
-  var thumbClass = 'gallery-list';
-  var thumbClick = window.ontouchstart === null ? 'touchstart' : 'click';
-  var mySwiper = new Swiper('#rooms .js-gallery-main', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-      el: '.gallery-lists',
-      type: 'custom',
-      renderCustom: function renderCustom(swiper, current, total) {
-        var slides = swiper.slides;
-        var html = '';
-
-        for (var i = 0; i < total; i++) {
-          if (current == i + 1) {
-            html = html + "<div class=\"".concat(thumbClass, " current\" data-slideto=\"").concat(i, "\">").concat(slides[i].innerHTML, "</div>");
-          } else {
-            html = html + "<div class=\"".concat(thumbClass, "\" data-slideto=\"").concat(i, "\">").concat(slides[i].innerHTML, "</div>");
-          }
-        }
-
-        return html;
-      }
-    }
-  });
-
-  var clickThumbs = function clickThumbs() {
-    var thumbItems = document.getElementsByClassName(thumbClass);
-
-    for (var i = 0; i < thumbItems.length; i++) {
-      thumbItems[i].addEventListener(thumbClick, function (e) {
-        var index = e.currentTarget.dataset.slideto;
-        mySwiper.slideTo(index, 500, true);
-      }, false);
-    }
-  };
-
-  clickThumbs();
-  mySwiper.on('slideChange', clickThumbs);
+window.addEventListener('load', function () {
+  setViewport();
+});
+window.addEventListener('resize', function () {
+  setViewport();
 });
